28bbed057edaf73db5ea07ef2c81f17ef6ae1596
RajeshEdayangat <rajesh@tuna.group>
2024-09-30 18:36:05 +0530

    adding new blog bug fix

--- src/components/AccordianDemo.tsx
+++ src/components/AccordianDemo.tsx
@@ -5,21 +5,21 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
-export default function AccordianDemo({
-  faq,
-}: {
-  faq: {
-    [x: string]: any;
-    question: string;
-    answer: string;
-  };
-}) {
+interface Faq {
+  question: string;
+  answer: string;
+}
+
+interface MainFaq {
+  faq: Faq[];
+}
+export default function AccordianDemo({ faq }: MainFaq) {
   return (
     <>
       <h3>Frequently Asked Questions (FAQs)</h3>
       {faq &&
-        faq.map((item: any) => (
-          <Accordion type="single" collapsible>
+        faq.map((item: Faq) => (
+          <Accordion type="single" collapsible key={item.question}>
             <AccordionItem value="item-1">
               <AccordionTrigger>{item.question}</AccordionTrigger>
               <AccordionContent>{item.answer}</AccordionContent>
